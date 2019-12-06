20f49a5738ca7b6e287bb21712bea16950484b6f
Graeme <graemster@gmail.com>
2019-12-05 18:14:16 -0600

    add Mongo config, jwt config, authorization

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
+require("./src/models/User");
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./src/routes/authRoutes");
+const requireAuth = require("./src/middlewares/requireAuth");
+const cors = require("cors");
+
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(authRoutes);
+if (process.env.mongoString) {
+  const mongoUri = process.env.mongoString;
+  mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  });
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to mongo instance");
+  });
+  mongoose.connection.on("error", err => {
+    console.error("Error connecting to mongo", err);
+  });
+}
 
-app.get('/', (req, res) => {
-  res.send('<h1>Greetings from the graeme-chat-app server!</h1>')
-})
+// app.post('/signin', (req, res) => {
+//   console.log("You just signed in!")
+// })
+// app.post('/signup', (req, res) => {
+//   console.log("You just signed up bruh???")
+// })
 
 io.on("connection", socket => {
-  console.log("a user connected :D");
+  console.log("a user connected to socket :D", socket);
   socket.on("join", ({ name, room }, callback) => {
     console.log(`user joined -- user: ${name}, room: ${room}`);
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("<h1>Greetings from the graeme-chat-app server!</h1>");
+  console.log("Get Root Route!");
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = mongoose.model("User");
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  // res.send("You made a post request on heroku!");
+  const { username, password } = req.body;
+  // const user = new User({ username, password });
+  // await user.save();
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.send({ token });
+    res.send("Succesfully created User");
+  } catch (err) {
+    // return res.send(err);
+    return res.status(422).send(err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(422).send({ error: "Must provide username and password" });
+  }
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(422).send({ error: "Invalid password or username" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password or username" });
+  }
+});
+
+module.exports = router;
